393734651bd3685c1ffc2f67737b2cc0d97189df
palage4a <vvnvvplg@gmail.com>
2022-09-27 14:35:34 +0300

    add super tab mapping

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -73,8 +73,15 @@ return require('packer').startup(function(use)
 	}
 	use { 'hrsh7th/nvim-cmp',
 		config = function()
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
 			local cmp = require('cmp')
 			if not cmp then return end
+			local luasnip = require('luasnip')
+			if not luasnip then return end
 			cmp.setup({
 				performance = {
 					debounce = 150,
@@ -83,22 +90,45 @@ return require('packer').startup(function(use)
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
 						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
 						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 					end,
 				},
 				window = {
-					-- completion = cmp.config.window.bordered(),
-					-- documentation = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
 				},
-				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
+				mapping = {
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				},
+				-- mapping = cmp.mapping.preset.insert({
+				-- 	['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				-- 	['<C-f>'] = cmp.mapping.scroll_docs(4),
+				-- 	['<C-Space>'] = cmp.mapping.complete(),
+				-- 	['<C-e>'] = cmp.mapping.abort(),
+				-- 	['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				-- }),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
 					-- { name = 'vsnip' }, -- For vsnip users.
@@ -172,15 +202,15 @@ return require('packer').startup(function(use)
 			require('nvim-autopairs').setup {}
 		end }
 
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        -- local ts_comment_integration = require('ts_context_commentstring.integrations.comment_nvim')
-        require('Comment').setup({
-          -- pre_hook = ts_comment_integration.create_pre_hook(),
-        })
-      end
-    }
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+			-- local ts_comment_integration = require('ts_context_commentstring.integrations.comment_nvim')
+			require('Comment').setup({
+				-- pre_hook = ts_comment_integration.create_pre_hook(),
+			})
+		end
+	}
 
 	use { 'tpope/vim-surround' }
 
@@ -212,4 +242,7 @@ return require('packer').startup(function(use)
 		"aserowy/tmux.nvim",
 		config = function() require("tmux").setup() end
 	})
+
+	-- require("luasnip.loaders.from_snipmate").lazy_load()
+	-- print('test')
 end)
