a20d08ba3c555b8cee1007bd10eadc9b51625785
palage4a <vvnvvplg@gmail.com>
2022-09-29 16:46:52 +0300

    update colorscheme

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,2 +1,4 @@
-vim.cmd 'colorscheme material'
-vim.g.material_style = "deep ocean"
+vim.opt.background = 'light'
+vim.cmd 'colorscheme sugarlily'
+-- vim.g.material_style = "deep ocean"
+-- vim.cmd 'colorscheme material'
