976eadbf0aeb1445b00aa31710945d32d57bf08a
palage4a <vvnvvplg@gmail.com>
2023-06-04 15:25:32 +0300

    rewind org-mode variables

--- emacs.d/custom/variables.el
+++ emacs.d/custom/variables.el
@@ -8,8 +8,16 @@
  '(custom-safe-themes
    '("d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" default))
  '(org-latex-compiler "pdflatex")
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(org-agenda-files '("/Users/i.palagecha/org/notes.org"))
+ '(org-agenda-start-on-weekday 1)
+ '(org-log-done 'note)
+ '(org-log-done-with-time t)
+ '(org-log-reschedule 'note)
+ '(org-refile-targets '((org-agenda-files :maxlevel . 5)))
  '(package-selected-packages
-   '(markdown-mode yasnippet yaml-mode xterm-color use-package typescript-mode solarized-theme org-gcal magit lua-mode jsonnet-mode js2-mode ivy-posframe go-mode exec-path-from-shell evil eglot counsel company)))
+   '(markdown-mode yasnippet yaml-mode xterm-color use-package typescript-mode solarized-theme org-gcal magit lua-mode jsonnet-mode js2-mode ivy-posframe go-mode exec-path-from-shell evil eglot counsel company))
+'(whitespace-line-column 120))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
