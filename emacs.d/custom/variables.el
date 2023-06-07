19f0c8044a987f9d91b4593c833065194a055383
palage4a <vvnvvplg@gmail.com>
2023-06-07 10:36:22 +0300

    update org-related variables

--- emacs.d/custom/variables.el
+++ emacs.d/custom/variables.el
@@ -7,17 +7,18 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" default))
- '(org-latex-compiler "pdflatex")
  '(ediff-split-window-function 'split-window-horizontally)
- '(org-agenda-files '("/Users/i.palagecha/org/notes.org"))
+ '(org-agenda-files '("/Users/i.palagecha/org/"))
  '(org-agenda-start-on-weekday 1)
+ '(org-image-actual-width '(500))
+ '(org-latex-compiler "pdflatex")
  '(org-log-done 'note)
  '(org-log-done-with-time t)
  '(org-log-reschedule 'note)
  '(org-refile-targets '((org-agenda-files :maxlevel . 5)))
  '(package-selected-packages
    '(markdown-mode yasnippet yaml-mode xterm-color use-package typescript-mode solarized-theme org-gcal magit lua-mode jsonnet-mode js2-mode ivy-posframe go-mode exec-path-from-shell evil eglot counsel company))
-'(whitespace-line-column 120))
+ '(whitespace-line-column 120))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
