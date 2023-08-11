03c72cdc42570480f32fb5d51bc4726e0c6e9221
palage4a <vvnvvplg@gmail.com>
2023-08-11 09:52:43 +0300

    disable terminus font

--- emacs.d/custom/general.el
+++ emacs.d/custom/general.el
@@ -29,7 +29,6 @@
 (setq indent-line-function 'insert-tab)
 (setq delete-trailing-lines nil)
 
-(set-frame-font "Terminus (TTF) 16" nil t)
 (setq-default cursor-type 'bar)
 (setq-default show-trailing-whitespace t)
 
