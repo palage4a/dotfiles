6429332ad736ec132982936581f5dd7249897664
palage4a <vvnvvplg@gmail.com>
2024-02-20 15:07:13 +0300

    initial commit

---
+++ nvim.back/init.lua
@@ -0,0 +1,13 @@
+vim.cmd [[source ~/.vimrc]]
+
+-- improve startup time
+-- require('impatient')
+
+require('plugins')
+require('keymaps')
+
+function _G.reload_package(name)
+    package.loaded[name] = nil
+    require(name)
+end
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,2 @@
-vim.cmd [[source ~/.vimrc]]
-
--- improve startup time
-require('impatient')
-
-require('plugins')
-require('keymaps')
-
-function _G.reload_package(name)
-    package.loaded[name] = nil
-    require(name)
-end
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
